6ddef3f470448073fe5a89504dd5742df1c05181
3eu <38431505+3eu@users.noreply.github.com>
2018-04-16 16:17:54 -0400

    Add files via upload

---
+++ BadBot/bot.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const Music = require('discord.js-musicbot-addon');
+ 
+const music = new Music(bot, {
+  youtubeKey: 'AIzaSyBg4TeEmyi7AfXFu5Gm86mcNJNSj65CkUA'
+});
+
+bot.login('NDM1NTMwMDIzMTQzODY2MzY4.DbaSdA.949RTPWm9f7Ut0sTp-u_0ZYnx2k');
